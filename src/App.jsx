0f503f4cf0a9701c3215300175b175046e1e151c
Ida <146117429+Idahel@users.noreply.github.com>
2025-05-13 13:17:59 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Movies from "./pages/Movies"
+import About from "./pages/About"
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </BrowserRouter>
+      
+    </>
+  )
+}
