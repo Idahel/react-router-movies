05696a339c852082ba0b7498460b8c6a8a72ec47
tildetilde <tilde.egebrand@gmail.com>
2025-05-14 16:28:53 +0200

    Improved styling

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { CollectionCard } from '../components/CollectionCard';
+import React from "react";
+import { CollectionCard } from "../components/CollectionCard";
+import "../styles/collections.css";
 
 export const Collections = () => {
   const curatedCollection = [
     {
-      id: 'women-filmmakers',
-      title: 'Women Filmmakers of Ukraine',
-      description: 'Discover powerful and poignant films directed by talented women from Ukraine, showcasing their unique perspectives and storytelling.',
+      id: "women-filmmakers",
+      title: "Women Filmmakers of Ukraine",
+      description:
+        "Discover powerful and poignant films directed by talented women from Ukraine, showcasing their unique perspectives and storytelling.",
       genre_ids: [],
-      personQuery: ''
+      personQuery: "",
     },
     {
-      id: 'documentaries',
-      title: 'Ukrainian Documentaries',
-      description: 'Explore the rich tapestry of Ukrainian life, history, and culture through compelling and insightful documentary films.',
+      id: "documentaries",
+      title: "Ukrainian Documentaries",
+      description:
+        "Explore the rich tapestry of Ukrainian life, history, and culture through compelling and insightful documentary films.",
       genre_ids: [99],
     },
-
   ];
 
   return (
-    <div>
-      <h1>Collections</h1>
-      <div>
-        {curatedCollection.map(collection => (
+    <div className="collections-container">
+      <h1 className="collections-title">Collections</h1>
+      <div className="collections-grid">
+        {curatedCollection.map((collection) => (
           <CollectionCard key={collection.id} collection={collection} />
         ))}
       </div>
     </div>
   );
-}
+};
