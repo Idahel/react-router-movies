85817df086229cfda82e3a578ff65f4e8ce8a376
Idahel <idakatrinhellgren@gmail.com>
2025-05-15 14:13:27 +0200

    changed from clicked card to only link the image and the title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,18 +2,22 @@ import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, from = "newest" }) => {
   return (
-    <Link to={`/movies/${movie.id}`} state={{ from }} className="movie-card">
+    <div className="movie-card">
       <div className="movie-image-container">
+        <Link to={`/movies/${movie.id}`} state={{ from }}>
           <img
             className="movie-image"
             src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
             alt={movie.title}
           />
+        </Link>
       </div>
       <div className="movie-content">
+      <Link to={`/movies/${movie.id}`} state={{ from }}>
         <h2 className="movie-title">{movie.title}</h2>
+        </Link>
         <p className="movie-director">Directed by {movie.director}</p>
       </div>
-      </Link>
+    </div>
   );
 };
