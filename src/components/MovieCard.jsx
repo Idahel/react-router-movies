8c84fd40622419df96efc1936aeeeed80153c3a7
Idahel <idakatrinhellgren@gmail.com>
2025-05-15 14:10:17 +0200

    made the whole movie-card clickable

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,20 +2,18 @@ import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, from = "newest" }) => {
   return (
-    <div className="movie-card">
+    <Link to={`/movies/${movie.id}`} state={{ from }} className="movie-card">
       <div className="movie-image-container">
-        <Link to={`/movies/${movie.id}`} state={{ from }}>
           <img
             className="movie-image"
             src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
             alt={movie.title}
           />
-        </Link>
       </div>
       <div className="movie-content">
         <h2 className="movie-title">{movie.title}</h2>
         <p className="movie-director">Directed by {movie.director}</p>
       </div>
-    </div>
+      </Link>
   );
 };
